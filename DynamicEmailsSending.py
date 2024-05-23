9dd5b011916cc64e1aed84ba2429bdddec9552f5
RoxieSalvatore <74130580+RoxieSalvatore@users.noreply.github.com>
2024-05-23 15:21:56 +0800

    Update DynamicEmailsSending.py
    erased my email

--- DynamicEmailsSending.py
+++ DynamicEmailsSending.py
@@ -6,7 +6,7 @@ from pathlib import Path
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'It was not me'
-email['to'] = 'roxanne.xiang@catontechnology.com'
+email['to'] = 'emailaddress'
 email['subject'] = '$1000'
 
 email.set_content(html.substitute(name = 'TinTin'),'html')
@@ -14,6 +14,6 @@ email.set_content(html.substitute(name = 'TinTin'),'html')
 with smtplib.SMTP(host = 'smtp.gmail.com', port=587) as smtp:
 	smtp.ehlo()
 	smtp.starttls()
-	smtp.login('roxanne.xiang.catontechnology@gmail.com', 'gcjl qkbq qeaw ketz')
+	smtp.login('some_gmail', 'gcjl qkbq qeaw ketz')
 	smtp.send_message(email)
 	print('all good boss!')
